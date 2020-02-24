d3a728a6c1e6d7e1b4cf9ed0f012ad5908a7b7fc
Fernando <fernando.eb@icloud.com>
2020-02-24 09:09:06 -0300

    primera clase

--- appleGuide/ContentView.swift
+++ appleGuide/ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, 130)
+            VStack(alignment: .leading) {
+                Text("Goto")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text("el gato flojo!")
+                        .foregroundColor(.blue)
+                        .font(.subheadline)
+                    Spacer()
+                    Text("dormido en el sillón").font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
     }
 }
 
